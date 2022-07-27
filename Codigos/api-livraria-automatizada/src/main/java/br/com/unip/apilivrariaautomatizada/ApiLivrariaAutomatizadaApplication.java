a8fee222b171825712eda3e8851715863558d4e4
cavalc00 <vinicius.caval00@gmail.com>
2022-07-27 20:48:00 -0300

    Projeto base e monografia

---
+++ Codigos/api-livraria-automatizada/src/main/java/br/com/unip/apilivrariaautomatizada/ApiLivrariaAutomatizadaApplication.java
@@ -0,0 +1,13 @@
+package br.com.unip.apilivrariaautomatizada;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiLivrariaAutomatizadaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiLivrariaAutomatizadaApplication.class, args);
+	}
+
+}
---
+++ Codigos/api-livraria-automatizada/src/test/java/br/com/unip/apilivrariaautomatizada/ApiLivrariaAutomatizadaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.unip.apilivrariaautomatizada;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiLivrariaAutomatizadaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
