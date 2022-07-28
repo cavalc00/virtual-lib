981a2a342124908d4f13930cb98c6d0376fde760
cavalc00 <vinicius.caval00@gmail.com>
2022-07-27 23:24:17 -0300

    Adicionado script para criacao de tabelas no banco de dados

--- Codigos/api-livraria-automatizada/src/main/resources/scripts/lib_db.sql
+++ Codigos/api-livraria-automatizada/src/main/resources/scripts/lib_db.sql
@@ -1,9 +1,46 @@
+CREATE TABLE GeneroLivro (
+	IdGenero BIGINT auto_increment not null,
+	Nome VARCHAR(100) not null,
+	primary key (IdGenero)
+) ENGINE=InnoDB;
+
+CREATE TABLE ResumoLivro (
+	IdResumo BIGINT auto_increment not null,
+	Descricao mediumtext not null,
+	primary key (IdResumo)
+) ENGINE=InnoDB;
+
 CREATE TABLE Livro (
-	Id BIGINT auto_increment not null,
+	IdLivro BIGINT auto_increment not null,
+	IdGenero BIGINT not null,
+	IdResumoLivro BIGINT not null,
 	Titulo VARCHAR(255) not null,
-	Autor VARCHAR(255) not null,
-	Editora VARCHAR(255) not null,
+	Autor VARCHAR(155) not null,
+	Editora VARCHAR(100) not null,
 	Ano INT null,
-	primary KEY(Id)
+	foreign key (IdGenero) references GeneroLivro(IdGenero),
+	foreign key (IdResumoLivro) references ResumoLivro(IdResumo),
+	primary KEY(IdLivro)
+) ENGINE=InnoDB;
+
+create table Usuario(
+	IdUsuario BIGINT auto_increment not null,
+	Nome VARCHAR(255) not null,
+	Cpf VARCHAR(14) not null,
+	Email VARCHAR(155) not null,
+	Login VARCHAR(155) not null,
+	Senha VARCHAR(10) not null,
+	primary key (IdUsuario)
+) ENGINE=InnoDB;
+
+create table LocacaoLivro(
+	IdLocacao BIGINT auto_increment not null,
+	IdUsuario BIGINT not null,
+	IdLivro BIGINT not null,
+	DataLocado Date null,
+	DataDevolvido Date null,
+	foreign key (IdUsuario) references Usuario(IdUsuario),
+	foreign key (IdLivro) references Livro(IdLivro),
+	primary key (IdLocacao)
 ) ENGINE=InnoDB;
 
