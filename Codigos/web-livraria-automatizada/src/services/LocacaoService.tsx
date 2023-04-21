f0a1babec4afa0a007ae148f6cf9a9a12a220c0d
rafaelso1 <rafaelso1@hotmail.com>
2023-04-21 15:58:09 -0300

    feat: books reserve

--- Codigos/web-livraria-automatizada/src/services/LocacaoService.tsx
+++
@@ -1,9 +0,0 @@
-import api from "../configs/api";
-
-function saveLocation(idLocacao: string) {
-  return api.post(`/locacao/${idLocacao}`);
-}
-
-const LocacaoService = { saveLocation };
-
-export default LocacaoService;
