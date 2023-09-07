76d73e65f1cb24fac5354103550e78fa7a8a3d52
cavalc00 <vinicius.caval00@gmail.com>
2023-09-07 12:09:35 -0300

    New features

--- Codigos/api-livraria-automatizada/src/main/java/br/com/unip/apilivrariaautomatizada/model/entity/Livro.java
+++ Codigos/api-livraria-automatizada/src/main/java/br/com/unip/apilivrariaautomatizada/model/entity/Livro.java
@@ -1,9 +1,23 @@
 package br.com.unip.apilivrariaautomatizada.model.entity;
 
 import br.com.unip.apilivrariaautomatizada.model.enums.FlagEnum;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 
 @Entity
 @Getter
@@ -41,4 +55,7 @@ public class Livro {
     @Column(name = "Flag")
     @Enumerated(value = EnumType.STRING)
     private FlagEnum flag;
+
+    @Column(name = "Prateleira")
+    private Integer prateleira;
 }
--- Codigos/api-livraria-automatizada/src/main/java/br/com/unip/apilivrariaautomatizada/service/ImageService.java
+++ Codigos/api-livraria-automatizada/src/main/java/br/com/unip/apilivrariaautomatizada/service/ImageService.java
@@ -10,16 +10,29 @@ import org.springframework.util.StringUtils;
 import org.springframework.web.server.ResponseStatusException;
 
 import javax.sql.rowset.serial.SerialBlob;
-import java.io.*;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.sql.SQLException;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Base64;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-import static br.com.unip.apilivrariaautomatizada.model.enums.ExtensionEnum.*;
+import static br.com.unip.apilivrariaautomatizada.model.enums.ExtensionEnum.IMAGE_JPEG;
+import static br.com.unip.apilivrariaautomatizada.model.enums.ExtensionEnum.IMAGE_JPG;
+import static br.com.unip.apilivrariaautomatizada.model.enums.ExtensionEnum.IMAGE_PNG;
 
 @Service
 @RequiredArgsConstructor
