3bff9084e1961e6255727e374aeeacb0e4157f8a
cavalc00 <vinicius.caval00@gmail.com>
2023-10-18 21:39:09 -0300

    done

--- Codigos/web-livraria-automatizada/src/pages/InfoBook/InfoBook.tsx
+++ Codigos/web-livraria-automatizada/src/pages/InfoBook/InfoBook.tsx
@@ -10,15 +10,23 @@ import Storage from "../../configs/Storage";
 function InfoBook() {
   const { id } = useParams();
   const [livro, setLivro] = useState<Livro>();
+  const [isUser, setIsUser] = useState<Boolean>(false);
   const localUser = Storage.getUser();
 
   useEffect(() => {
     getBookById();
+    getIsUser(id, localUser.email);
   }, [id]);
 
   function getBookById() {
     LivroService.findById(id)
       .then((response) => setLivro(response.data))
+      .catch((error) => console.log(error));
+  }
+
+  function getIsUser(idLivro: any, email: string) {
+    LocacaoService.isReservedByUser(idLivro, email)
+      .then((response) => setIsUser(response.data))
       .catch((error) => console.log(error));
   }
 
@@ -43,7 +51,7 @@ function InfoBook() {
     window.location.reload();
   }
 
-  function updateReserve(idLivro: string){
+  function updateReserve(idLivro: string) {
     LocacaoService.returnBook(idLivro);
     window.location.reload();
   }
@@ -53,7 +61,7 @@ function InfoBook() {
       {renderPreviewImage()}
       <Card className="book-details">
         <div className="title-badge">
-        <h4>Prateleira: {livro?.prateleira}</h4>
+          <h4>Prateleira: {livro?.prateleira}</h4>
           <div className="badge">
             {livro?.flag === "DISPONIVEL" ? (
               <Badge pill bg="success">
@@ -96,10 +104,13 @@ function InfoBook() {
               >
                 Reservar Livro
               </Button>
-            ) : livro?.flag === "RESERVADO" && localUser != null ? (
-              <Button variant="btn btn-primary" onClick={() => {
-                updateReserve(livro.id);
-              }}>
+            ) : livro?.flag === "RESERVADO" && localUser != null && isUser ? (
+              <Button
+                variant="btn btn-primary"
+                onClick={() => {
+                  updateReserve(livro.id);
+                }}
+              >
                 Devolver
               </Button>
             ) : (
--- Codigos/web-livraria-automatizada/src/services/LocacaoService.tsx
+++ Codigos/web-livraria-automatizada/src/services/LocacaoService.tsx
@@ -8,6 +8,24 @@ function returnBook(idLivro: string) {
   return api.put(`/locacao/${idLivro}`);
 }
 
-const LocacaoService = { reserveBook, returnBook };
+function isReservedByUser(idLivro: string, email: string){
+  const myParams = new URLSearchParams();
+
+  if (idLivro) {
+    myParams.append("idBook", idLivro);
+  }
+
+  if (email && email !== " ") {
+    myParams.append("email", email);
+  }
+
+  const request = {
+    params: myParams,
+  };
+
+  return api.get<Boolean>(`/locacao/compare`, request);
+}
+
+const LocacaoService = { reserveBook, returnBook, isReservedByUser};
 
 export default LocacaoService;
